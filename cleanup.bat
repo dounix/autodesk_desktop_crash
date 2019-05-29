5d324a20cd695bedad8447be4c48bd2c4c1bad73
dounix <jims@ape.camptaylor.net>
2019-05-29 10:42:58 -0400

    lots of echosss

--- cleanup.bat
+++ cleanup.bat
@@ -1,13 +1,37 @@
+@echo off
+echo "This will attempt to uninstall and clean the autodesk desktop app"
+echo "It follows the process in https://knowledge.autodesk.com/search-result/caas/sfdcarticles/sfdcarticles/Install-Autodesk-Desktop-App.html"
+echo
+echo "press enter to continue, or close this window to abort"
+pause
+
+echo "stopping the autodesk service..."
+echo "...press enter to continue"
+pause
 net stop AdAppMgrSvc
+
+echo "killing the running autodesk desktop processes"
+echo "...press enter to continue"
+pause
 
 taskkill /IM "AdAppMgrSvc.exe" /F
 taskkill /IM "AdAppMgrUpdater.exe" /F
 
-
+echo "cleaning up Autodesk desktop files"
+echo "...press enter to continue"
+pause
 del C:\Programdata\Autodesk\AutodeskDesktopApp.ini
-del  "%APPDATA%\Autodesk\Autodesk Desktop App\AutodeskDesktopApp"
-del  "%LOCALAPPDATA%\Autodesk\Web Services\LoginState.xml"
+del "%APPDATA%\Autodesk\Autodesk Desktop App\AutodeskDesktopApp"
+del "%LOCALAPPDATA%\Autodesk\Web Services\LoginState.xml"
 
+echo "removing registry entries"
+echo "...press enter to continue"
+pause
 regedit /i registryclean.reg
 
+
+echo "This will start a browser to download the Autodesk desktop app"
+echo "...press enter to continue, and proceed to reinstall"
 start https://emsfs.autodesk.com/utility/adskapp/landing/files/AdskappBootstrap/Autodesk_Desktop_App_Bootstrap.exe
+
+
